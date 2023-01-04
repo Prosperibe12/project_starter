a8a5a4eedf460f24a33a995e4841775ff1e599c7
Prosperibe12 <prosperibe12@gmail.com>
2023-01-04 22:24:04 +0100

    create custom user model

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,83 @@
 from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, UserManager, PermissionsMixin)
+from django.utils.translation import gettext_lazy as _ 
 
-# Create your models here.
+from auditlog.registry import auditlog
+
+from backend.helper.models import HelperModel 
+
+class MyUserManager(UserManager):
+    '''
+    A buse user manager class
+    '''
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given email, and password.
+        """
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self,email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+class User(HelperModel,AbstractBaseUser,PermissionsMixin):
+    """
+    A base class implementing a fully featured User model with
+    admin-compliant permissions.
+    """
+    first_name = models.CharField(_("first name"), max_length=150, blank=False, null=False)
+    last_name = models.CharField(_("last name"), max_length=150, blank=False, null=False)
+    email = models.EmailField(_("email address"), blank=False, null=False, unique=True)
+    address = models.CharField(_("Address"), blank=True, null=True, max_length=250)
+    city = models.CharField(_("City"), blank=True, null=True, max_length=250)
+    country = models.CharField(_("Country"), blank=True, null=True, max_length=250)
+    is_verified = models.BooleanField(
+        _("verified"),
+        default=False,
+        help_text=_(
+            "Designates whether this user is active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    objects = MyUserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name","last_name"]
+    
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+auditlog.register(model=User, exclude_fields=['password', 'last_login'])
