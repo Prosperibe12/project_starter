e933d2b3888fb718bfdef4d7ce742c8dbf54cd25
Prosperibe12 <prosperibe12@gmail.com>
2023-01-05 23:43:07 +0100

    email verification view

--- backend/authentications/urls.py
+++ backend/authentications/urls.py
@@ -4,4 +4,5 @@ from backend.authentications import views
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
+    path('verify_email/', views.VerifyEmail.as_view(), name='verify-email'),
 ]
--- backend/authentications/views.py
+++ backend/authentications/views.py
@@ -1,15 +1,20 @@
+from django.contrib.sites.shortcuts import get_current_site 
+
 from rest_framework import generics
 from rest_framework import status 
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from backend import utils 
 from backend.authentications import serializers
 from backend import models 
+from backend.authentications.utils import RegisterEmailNotification
 
 class RegisterView(generics.GenericAPIView):
     '''
-    A class that registers a new user
+    A class that registers a new User
     '''
     authentication_classes = ()
+    permission_classes = ()
     serializer_class = serializers.RegisterSerializer
     
     def post(self, request):
@@ -17,5 +22,20 @@ class RegisterView(generics.GenericAPIView):
         serializer_data = self.serializer_class(data=data)
         if serializer_data.is_valid(raise_exception=True):
             serializer_data.save()
-            return utils.CustomResponse.Success(serializer_data.data, status=status.HTTP_201_CREATED)
+            
+            # send email notification and link for account activation
+            domain_name = get_current_site(request).domain
+            user = models.User.objects.get(email=serializer_data.data['email'])
+            token = RefreshToken.for_user(user).access_token
+            email_message = RegisterEmailNotification(domain_name, user, token).send_email_notification()
+            if email_message:
+                return utils.CustomResponse.Success(serializer_data.data, status=status.HTTP_201_CREATED)
         return utils.CustomResponse.Failure(serializer_data.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class VerifyEmail(generics.GenericAPIView):
+    
+    authentication_classes = ()
+    permission_classes = ()
+    
+    def get(self, request):
+        pass 
